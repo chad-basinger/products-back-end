4863081c2bbb70ea45a47e0d023be640c7d0f200
Chad Basinger <chad.basingeris@gmail.com>
2021-03-19 16:48:51 -0600

    on step 4

---
+++ db/create_product.sql
@@ -0,0 +1,3 @@
+insert into product_id (name, description, price, image_url)
+values 
+($1, $2, $3, $4)
---
+++ server/products_controllers.js
@@ -0,0 +1,17 @@
+module.exports = {
+    create: (req, res) => {
+
+    },
+    getOne: (req, res) => {
+
+    },
+    getAll: (req, res) => {
+
+    },
+    update: (req, res) => {
+
+    },
+    delete: (req, res) => {
+        
+    }
+}
